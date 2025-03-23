85fb98db83bdf1d057a8b3b5fd216c588cc126c0
Advay Singh <advay@umich.edu>
2025-03-23 16:39:46 -0400

    Added copying script

---
+++ utils/copy_svelte_files.sh
@@ -0,0 +1,26 @@
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <username> <remote_path>"
+    exit 1
+fi
+
+USERNAME=$1
+REMOTE_PATH=$2
+LOCAL_PATH=~/470/gui_debugger/
+SERVER="login-course.engin.umich.edu"
+
+# Create local directory if it doesn't exist
+mkdir -p "$LOCAL_PATH"
+
+# Create temporary script for SFTP commands
+SFTP_COMMANDS=$(mktemp)
+echo "cd $REMOTE_PATH" > "$SFTP_COMMANDS"
+echo "lcd $LOCAL_PATH" >> "$SFTP_COMMANDS"
+echo "get *_svelte" >> "$SFTP_COMMANDS"
+
+# Execute SFTP with commands
+sftp -b "$SFTP_COMMANDS" "$USERNAME@$SERVER"
+
+# Clean up
+rm "$SFTP_COMMANDS"
+
+echo "Files ending with '_svelte' have been copied to $LOCAL_PATH"
